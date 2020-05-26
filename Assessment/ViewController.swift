930e659b7142c8977d65d99a9db707f4bb7ccd16
ibraheemabuhadba51@gmail.com <Rock0797023426>
2020-05-26 04:28:05 +0300

    Initial Commit

---
+++ Assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assessment
+//
+//  Created by Heema Boss on 5/26/20.
+//  Copyright © 2020 Ibraheem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
